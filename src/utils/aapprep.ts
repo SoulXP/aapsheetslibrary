e61147f66e327bc9ba6ba01ee45d75d39474d96a
SoulXP <snowflake1194@gmail.com>
2022-02-15 23:26:23 +0200

    WIP collapsing prep log entries to count entries per employee, per date

--- src/utils/aapprep.ts
+++ src/utils/aapprep.ts
@@ -1,10 +1,37 @@
 // Types
-export type prepentry_t = [Date, number, string, Date, Date, Date];
-export type entrycount_t = [string, Date, number];
+export type prepentry_t = [Date, number | string, string, Date, Date, Date, Date];
+export type entry_t = {
+    date: Date,
+    location: number | string,
+    employee: string,
+    start: Date,
+    tcin: Date,
+    tcout: Date,
+    tcdur: Date,
+    end: Date,
+};
+
+// Prep log constants
+const AAP_PREPLOG_V1_ENTRIES = 6;
+const AAP_PREPLOG_V2_ENTRIES = 10;
 
 // Functions
-export function count_prep_entries(entries: prepentry_t[]): entrycount_t[] {
+export function count_prep_entries(entries: prepentry_t[]): prepentry_t[] {
     if (entries.length === 0) return [];
+    if (entries[0].length % AAP_PREPLOG_V1_ENTRIES !== 0) throw '[ERROR] insufficient prep entries';
+
+    // Collapse columns to total prep entries
+    let i: number = 0;
+    const collapsed_entries: prepentry_t[] = [];
+    const entry: entry_t = { date: new Date(), location: 0, employee: '', start: new Date(), end: new Date(), tcin: new Date(), tcout: new Date(), tcdur: new Date() };
+    for (const r of entries) {
+        let { date, location, employee, start, end, tcin, tcout, tcdur }: entry_t = entry;
+        for (const c of r) {
+            switch (i) {
+                case 0: date = c; break;
+            }
+        }
+    }
 
     // Sort by date
     const sorted_entries = [...entries].sort((a: prepentry_t, b: prepentry_t): number => {
@@ -20,44 +47,5 @@ export function count_prep_entries(entries: prepentry_t[]): entrycount_t[] {
         return 1;
     });
 
-    // Count date occurrences
-    let last_date: Date = new Date();
-    let last_employee: string = '';
-    let count: number = 0;
-    let reduced: entrycount_t[] = [];
-    let entry: entrycount_t = [last_employee, last_date, count];
-
-    for (const r of sorted_entries) {
-        const date: Date = r[0];
-        const employee: string = r[2];
-
-        if (last_employee === '') {
-            last_date = date;
-            last_employee = employee;
-            count++;
-            continue;
-        }
-
-        if (employee === last_employee) {
-            entry[0] = employee;
-            
-            if (date === last_date) {
-                count++;
-            }
-
-            entry[1] = date;
-            entry[2] = count;
-            
-            continue;
-        }
-
-        reduced.push(entry);
-        count = 0;
-        last_employee = employee;
-        last_date = date;
-        entry = [last_employee, last_date, count];
-    }
-    
-    Logger.log(`Date: ${sorted_entries[0][0]}`);
-    return reduced;
+    return sorted_entries;
 }
